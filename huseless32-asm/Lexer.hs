08a154dd5c21a687f1fd7af86b75d72faf0867fd
Enrico Lumetti <enrico.lumetti@gmail.com>
2015-04-29 20:42:30 +0200

    Upload Lexer.hs

---
+++ huseless32-asm/Lexer.hs
@@ -0,0 +1,44 @@
+module Lexer where
+
+import Data.Char (digitToInt)
+import Text.Parsec
+import Text.Parsec.Combinator
+import Text.Parsec.Char
+import Text.Parsec.String (Parser)
+import Text.Parsec.Language (emptyDef)
+
+import qualified Text.Parsec.Token as Tok
+
+lexer :: Tok.TokenParser ()
+lexer = Tok.makeTokenParser style
+    where
+        ops = ["+", "-", "*"]
+        --names = [],
+        style = emptyDef {
+            Tok.commentLine = ";"
+            }
+
+-- RIPOFF, thanks Parsec.Token <3
+bnumber base baseDigit
+        = do{ digits <- many1 baseDigit
+            ; let n = foldl (\x d -> base*x + toInteger (digitToInt d)) 0 digits
+            ; seq n (return n)
+            }
+
+hex_literal = char '$' *> bnumber 16 hexDigit
+dec_literal = bnumber 10 digit
+
+read_sign = do
+    p <- char '+' <|> char '-'
+    if p == '+'
+       then return id
+       else return negate
+
+
+unsigned_literal = hex_literal <|> dec_literal
+
+integer :: Parser Integer
+integer = Tok.lexeme lexer (
+    ($) <$> option id read_sign <*> unsigned_literal <?> "integer"
+    )
+
