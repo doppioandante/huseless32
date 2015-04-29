eb54c738a31728f6b00872d7872a1f1db912f83d
Enrico Lumetti <enrico.lumetti@gmail.com>
2015-04-29 20:40:48 +0200

    I can parse an integer literal

---
+++ huseless32-asm/Syntax.hs
@@ -0,0 +1,46 @@
+module Syntax where
+
+data AsmLine = Instruction Label InstructionBody
+             | PseudoInstruction Label PseudoInstructionBody
+             | Assignment Variable Expression
+             deriving (Eq, Show)
+
+data Label = Label String
+           | EmptyLabel
+           deriving (Eq, Show)
+
+data InstructionBody = Mnemonic0 String -- HALT, NOP, RESET, TRAP, TRACE,...
+                     | Mnemonic1 String Operand -- MOVTOSTR, PUSH, POP..
+                     | Mnemonic2 String Operand Operand -- MOV, MVL
+                     deriving (Eq, Show)
+
+data PseudoInstructionBody = Declare Size Expression
+                           | Initialize Size InitList
+    deriving (Eq, Show)
+
+data Variable = Variable String deriving (Eq, Show)
+
+-- TODO
+data Expression = Expression Int deriving (Eq, Show)
+
+data Operand = Direct    Register
+             | Immediate Expression
+             | Absolute  Expression
+             | Indirect  Register
+             | IndirectOffset Expression Register
+             | RelativeOffset Expression
+             | Decrementing   Register
+             | Incrementing   Register
+             | Device Expression
+             deriving (Eq, Show)
+
+data Size = SizeByte | SizeWord | SizeLWord deriving (Eq, Show)
+
+data InitList = InitList InitValue [InitValue]
+              deriving (Eq, Show)
+
+data InitValue = InitASCII String
+               | InitExpr  Expression
+               deriving (Eq, Show)
+
+data Register = Register Int deriving (Eq, Show)
